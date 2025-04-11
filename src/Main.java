c1842d143b67de0155bfc6be578a336262e4448e
Viktor Shidlovskiy <v.shidlovskiy80@gmail.com>
2025-04-11 15:28:43 +0400

    new fuctional-operation with number

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,20 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-                 String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        System.out.println("Введите первое целое число: ");
+        int firstNum = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе целое число: ");
+        int secondNum = new Scanner(System.in).nextInt();
+
+        int amount = firstNum+secondNum;
+        int difference = firstNum-secondNum;
+        int multiplication = firstNum*secondNum;
+        double quotient = (double) firstNum/ secondNum;
+
+        System.out.println("Сумма чисел = " + amount);
+        System.out.println("Разность чисел = " + difference);
+        System.out.println("Произведение чисел = " + multiplication);
+        System.out.println("Частное чисел = " + quotient);
+
     }
 }
