cc809584dea205c5a566b35eb47af98a918f8568
Viktor Shidlovskiy <v.shidlovskiy80@gmail.com>
2025-04-18 01:02:09 +0400

    new fuctional find file path

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,32 @@
 import java.util.Scanner;
+import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-                 String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        int validFilesCount = 0;
+
+        // Бесконечный цикл опроса пути к файлу
+        while (true) {
+            System.out.print("Укажите путь к файлу: ");
+
+            String path = new Scanner(System.in).nextLine(); // Получаем введённый путь от пользователя
+
+            File file = new File(path); // Создаем объект класса File для проверки существования файла
+
+            if (!file.exists()) {      // Если файл не существует
+                System.out.println("Файл не найден!!!");
+                continue;              // Продолжаем цикл заново
+            }
+
+            if (file.isDirectory()) {  // Если путь ведет к директории (папке)
+                System.out.println("Указанный путь ведёт к папке, укажите путь к файлу!!!");
+                continue;              // Продолжаем цикл заново
+            }
+
+            // Иначе (файл существует и не является директорией):
+            validFilesCount++;       // Увеличиваем счётчик правильно введённых файлов
+            System.out.println("Путь указан верно");
+            System.out.printf("Это файл номер %d\n", validFilesCount); // Выводим номер файла
+        }
     }
 }
